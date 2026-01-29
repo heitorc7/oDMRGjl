47dfaf11235d6e38b6b12abb552c592ba4b0b16e
heitorc7 <heitorc.7@gmail.com>
2026-01-29 14:42:36 +0900

    fix: remove erroneous global variables in warmUp function

--- src/funcs/warmUp.jl
+++ src/funcs/warmUp.jl
@@ -1,18 +1,16 @@
 function warmUp(rho::MPS, LdL::MPO, convergenceThreshold = 0.00001, cutoff = 1E-10)
     println("Initializing warm-up routine")
-    global energyFin = floatmax(Float64)
-    global warmUpTag = true
-    # convergenceThreshold = 0.00001 #1E-10
-    global warmUpTracker = 0
-    while warmUpTag
-        global warmUpTracker += 1
+    energyFin = floatmax(Float64)
+    warmUpTracker = 0
+    while true
+        warmUpTracker += 1
         energyIni = energyFin;
-        global energyFin, rho = dmrg(LdL,rho; nsweeps=1, maxdim=1, cutoff=cutoff, outputlevel=0)
+        energyFin, rho = dmrg(LdL,rho; nsweeps=1, maxdim=1, cutoff=cutoff, outputlevel=0)
     
         # println("Difference between energies: ", (energyIni-energyFin))
         if (abs(energyFin) < abs(energyIni)) && abs(abs(energyFin) - abs(energyIni)) < convergenceThreshold
-            global warmUpTag = false
             println("EndedWarmUpAfter ", warmUpTracker, " sweeps")
+            break
         end
     end
     return rho
